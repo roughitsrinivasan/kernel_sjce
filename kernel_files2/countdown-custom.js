6142adc2cf28d947825c0c36df88dedc6785158f
roughitsrinivasan <81728997+roughitsrinivasan@users.noreply.github.com>
2023-07-04 10:17:37 +0530

    Delete kernel_files2 directory

--- kernel_files2/countdown-custom.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function() {
-        $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2022, 4, 20, 8)}); // year, month, date, hour
-        });
-});		
-
